865c9c47ba074bea8eabfa70d5299e763410675d
yuli-r <iuliana.rotaru234@gmail.com>
2022-01-09 13:39:41 +0000

    Solve repeatString TDD task

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num === 0) {
+        return("");
+    } else if (num < 0) {
+        return("ERROR");
+    } else {
+      let repeatString = "";
+      while (num > 0) {
+        repeatString += string;
+        num--;
+      }
+      return(repeatString);
+    }
 };
 
 // Do not edit below this line
