16202aa1f0b573e02abe856eb2ab066afcbf5f16
yuli-r <iuliana.rotaru234@gmail.com>
2022-01-26 18:08:39 +0000

    Complete exercise 09 - palindromes

--- top-js-exercises/09_palindromes/palindromes.js
+++ top-js-exercises/09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+const palindromes = function (a) {
+    let splitString = a.toLowerCase().split('');
+    let initialString = splitString.join('').replace(/[.,\/#!" "$%\^&\*;:{}=\-_`~()]/g, "");
+    splitString = initialString.split('');
+    let reverseString = splitString.reverse();
+    let joinString = reverseString.join('');
 
-};
+    return ((initialString === joinString) ? true : false);
+  }
 
 // Do not edit below this line
 module.exports = palindromes;
