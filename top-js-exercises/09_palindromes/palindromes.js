8d57067be92eb9249c3235396f1e01e5d2fefada
yuli-r <iuliana.rotaru234@gmail.com>
2022-01-27 15:15:25 +0000

    Commit to remove the Contains emphasized items message

--- top-js-exercises/09_palindromes/palindromes.js
+++ top-js-exercises/09_palindromes/palindromes.js
@@ -1,12 +1,12 @@
 const palindromes = function (a) {
-    let splitString = a.toLowerCase().split('');
-    let initialString = splitString.join('').replace(/[.,\/#!" "$%\^&\*;:{}=\-_`~()]/g, "");
-    splitString = initialString.split('');
-    let reverseString = splitString.reverse();
-    let joinString = reverseString.join('');
-
-    return ((initialString === joinString) ? true : false);
-  }
+  processedString = a.toLowerCase().replace(/[.,\/#!" "$%\^&\*;:{}=\-_`~()]/g, "");
+  return (
+    processedString
+    .split('')
+    .reverse()
+    .join('') == processedString
+  );
+}
 
 // Do not edit below this line
 module.exports = palindromes;
