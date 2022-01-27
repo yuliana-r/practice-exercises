e2323b600dbd39fe28f10a8bc3daa5e5887db511
yuli-r <iuliana.rotaru234@gmail.com>
2022-01-27 16:21:11 +0000

    Complete exercise 12 - findTheOldest

--- top-js-exercises/12_findTheOldest/findTheOldest.js
+++ top-js-exercises/12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
 
-};
+    const currentYear = new Date().getFullYear();
 
+    array.map(item => {
+        if (item.yearOfDeath !== undefined) {
+            item.Age = item.yearOfDeath - item.yearOfBirth;
+        } else {
+            item.Age = currentYear - item.yearOfBirth;
+        }
+    })
+
+    const oldest = array.sort(function (a, b) {
+        const lastPerson = a.Age;
+        const nextPerson = b.Age;
+
+        return (lastPerson > nextPerson) ? -1 : 1;
+
+    })
+
+    return oldest[0];
+}
 // Do not edit below this line
 module.exports = findTheOldest;
