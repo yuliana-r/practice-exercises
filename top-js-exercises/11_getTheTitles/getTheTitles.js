5333a2705c9e1e61f73197d12c3cbde4eb5594fa
yuli-r <iuliana.rotaru234@gmail.com>
2022-01-27 16:22:05 +0000

    Update folder

--- top-js-exercises/11_getTheTitles/getTheTitles.js
+++ top-js-exercises/11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function (array) {
-    return array.map(item => item.title);
-};
+const findTheOldest = function (array) {
+    array.map(item => item.Age = item.yearOfDeath - item.yearOfBirth);
+
+    let oldest = array.reduce((max, person) => max.Age > person.Age ? max : person);
+    return oldest.name;
+  };
 
 // Do not edit below this line
 module.exports = getTheTitles;
