a269edc3349ba6206f635d3c6805510f0f098676
yuli-r <iuliana.rotaru234@gmail.com>
2022-01-27 15:04:21 +0000

    Complete exercise 10 - fibonacci

--- top-js-exercises/10_fibonacci/fibonacci.js
+++ top-js-exercises/10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
 
-};
+    if (n <= 0) return "OOPS";
+
+    if (n == 1 || n == 2) {
+      return 1;
+    }
+
+    return fibonacci(n - 2) + fibonacci(n - 1);
+  };
 
 // Do not edit below this line
 module.exports = fibonacci;
