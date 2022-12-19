81073edeb9fb2b764c992cbf05e348ad133f81a4
yuli-r <iuliana.rotaru234@gmail.com>
2022-12-19 20:14:05 +0000

    Complete ex. 2-4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    let newString = '';
+
+    if (num < 0) {
+        return 'ERROR';
+    }
+    
+    for (let i = 0; i < num; i++) {
+        newString += string;
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
 
+    const array = args[0];
+
+    return array.filter(value => !args.includes(value));
 };
 
 // Do not edit below this line
