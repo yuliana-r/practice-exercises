f1a11c9491ce8cb3cc084353e483ebaa4b4c0f6c
yuli-r <iuliana.rotaru234@gmail.com>
2022-12-19 16:37:59 +0000

    Add exercise files

--- top-js-exercises/03_reverseString/reverseString.js
+++
@@ -1,8 +0,0 @@
-const reverseString = function(str) {
-    const myArray = str.split("");
-    let newString = myArray.reverse().join("");
-    return(newString);
-};
-
-// Do not edit below this line
-module.exports = reverseString;
--- top-js-exercises/11_getTheTitles/getTheTitles.js
+++
@@ -1,9 +0,0 @@
-const findTheOldest = function (array) {
-    array.map(item => item.Age = item.yearOfDeath - item.yearOfBirth);
-
-    let oldest = array.reduce((max, person) => max.Age > person.Age ? max : person);
-    return oldest.name;
-  };
-
-// Do not edit below this line
-module.exports = getTheTitles;
