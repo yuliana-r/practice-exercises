b1ce7b88131f0771b1649a44413509df29d74325
yuli-r <iuliana.rotaru234@gmail.com>
2022-12-19 20:25:12 +0000

    Complete ex.6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    if (a < 0 || b < 0 || typeof a !== 'number' || typeof b !== 'number') {
+        return 'ERROR';
+    }
+
+    let sum = 0;
+
+    if (a > b) {
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
