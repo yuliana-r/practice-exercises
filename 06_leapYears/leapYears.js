e512a5fecaa7f1bd6b9a023a35db68fc98ecf2b2
yuli-r <iuliana.rotaru234@gmail.com>
2023-01-09 15:10:44 +0000

    Complete temp conversion exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,19 +1,21 @@
 const leapYears = function(year) {
 
     // return (year % 100 == 0 && year % 400 == 0 || year % 4 == 0 || year % 400 == 0);
-    if (year % 100 == 0) {
-        if (year % 400 == 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+    // if (year % 100 == 0) {
+    //     if (year % 400 == 0) {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // }
 
-    if (year % 4 == 0) {
-        return true;
-    } else {
-        return false;
-    }
+    // if (year % 4 == 0) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 };
 
 //Leap years are years divisible by four (like 1984 and 2004) 4
