b6561f5155c1b0e320276f63cec344d8fd0fa034
yuli-r <iuliana.rotaru234@gmail.com>
2023-01-10 20:54:09 +0000

    Complete titles exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -15,5 +15,6 @@ const fibonacci = function(count) {
     return b;
 };
 
+
 // Do not edit below this line
 module.exports = fibonacci;
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let titles = [];
 
+    for (let book of books) {
+        titles.push(book.title);
+    }
+
+    return titles;
 };
 
 // Do not edit below this line
