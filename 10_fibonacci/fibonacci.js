e5aebab539c10840309fee9cf1483ca017e4297e
yuli-r <iuliana.rotaru234@gmail.com>
2023-01-10 20:42:57 +0000

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+    if (count < 0) return 'OOPS';
 
+    if (count == 0) return 0;
+
+    let a = 0;
+    let b = 1;
+
+    for (let i = 1; i < count; i++) {
+        let temp = b;
+        b = a + b;
+        a = temp;
+    }
+
+    return b;
 };
 
 // Do not edit below this line
