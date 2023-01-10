437e345cb13bb952011729be75be232fc9fa6b8f
yuli-r <iuliana.rotaru234@gmail.com>
2023-01-10 20:25:41 +0000

    Complete palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    const newWord = word.toLowerCase().replace(/[^a-z]/g, "");
+
+    return newWord == newWord.split('').reverse().join('');
 };
 
 // Do not edit below this line
