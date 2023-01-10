d30f778aba0afba5880ec1ec83b58e7e78989446
yuli-r <iuliana.rotaru234@gmail.com>
2023-01-10 21:07:40 +0000

    Complete find oldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    
+    const getAge = function(birth, death) {
+        if (!death) {
+            death = new Date().getFullYear();
+        }
+
+        return death - birth;
+    }
+    return people.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge > currentAge ? oldest : currentPerson;
+    })
 
 };
 
