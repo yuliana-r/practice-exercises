e989c87c0d16b2926ea5e091e674e2330322c667
yuli-r <iuliana.rotaru234@gmail.com>
2023-06-06 18:01:53 +0100

    Tidy up recursion folder

--- comp sci/recursion/power-func.js
+++
@@ -1,6 +0,0 @@
-function power(n, m) {
-    if (m == 0) {
-        return 1;
-    }
-    return n * power(n, m - 1);
-}
--- comp sci/recursion/sum-range.js
+++
@@ -1,6 +0,0 @@
-function sumRange(n) {
-    if (n === 1) {
-        return 1;
-    }
-    return n + sumRange(n-1);
-}
