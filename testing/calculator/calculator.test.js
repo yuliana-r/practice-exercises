f9bc9e485c4f8d0af291a8984ebb22d961b75099
yuli-r <iuliana.rotaru234@gmail.com>
2023-06-27 17:38:04 +0100

    Add calculator tests

---
+++ testing/calculator/calculator.test.js
@@ -0,0 +1,21 @@
+const calculator = require('./calculator');
+
+test('adds 2 numbers', () => {
+    expect(calculator.add(5, 7)).toEqual(12);
+});
+
+test('subtracts 2 numbers', () => {
+    expect(calculator.subtract(10, 4)).toEqual(6);
+});
+
+test('multiplies 2 numbers', () => {
+    expect(calculator.multiply(3, 5)).toEqual(15);
+});
+
+test('divides 2 numbers', () => {
+    expect(calculator.divide(27, 3)).toEqual(9);
+});
+
+test('does not divide by 0', () => {
+    expect(calculator.divide(4, 0)).toEqual('Cannot divide by 0');
+});
