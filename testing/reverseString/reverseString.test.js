33d67d7c5675905c919203fd5c873dae4dd89439
yuli-r <iuliana.rotaru234@gmail.com>
2023-06-27 17:24:26 +0100

    Add reverseString tests

---
+++ testing/reverseString/reverseString.test.js
@@ -0,0 +1,10 @@
+const reverseString = require('./reverseString');
+
+test('reverses single word string', () => {
+    expect(reverseString('kitty')).toEqual('yttik');
+});
+
+test('reverses multiple words', () => {
+    expect(reverseString('cats are the best')).toEqual('tseb eht era stac');
+});
+
